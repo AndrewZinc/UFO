f2ce9fde774759e868efe5816f3557aa4738077f
AndrewZinc <107150022+AndrewZinc@users.noreply.github.com>
2022-10-04 13:47:41 -0400

    Added the buildTable function to the js file.

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+function buildTable(data){
+    // empty the table
+    tbody.html("");
+    // loop through the data - append a row and colunns
+    data.forEach((dataRow) =>{
+        let row = tbody.append("tr");
+        // loop through the data - append the data values to the row.
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
